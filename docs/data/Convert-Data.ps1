f70f5592e4d8d173b27967be6b4edab780138e40
Tomilov Alexander <sopholos@sopholos.ru>
2023-10-23 16:07:50 +0500

    refactor data and quadrants

--- docs/data/Convert-Data.ps1
+++ docs/data/Convert-Data.ps1
@@ -1,10 +1,20 @@
+$ErrorActionPreference = "Stop"
+
 $scriptDir = Split-Path $PSCommandPath
 
-$data = Import-Csv $scriptDir/Technologies.tsv -Delimiter "`t"
+$data = Import-Csv $scriptDir/Technologies.tsv -Delimiter "`t" | ? label -ne ""
+
+$rings = @{
+    'ADOPT'     = 0
+    'TRIAL'     = 1
+    'ASSESS'    = 2
+    'HOLD'      = 3
+}
+
 foreach ($d in $data) {
     $d.active = $d.active.ToLower()
     $d.quadrant = [int]$d.quadrant
-    $d.ring = [int]$d.ring
+    $d.ring = [int]$rings[$d.ring]
     $d.moved = [int]$d.moved
     $d.active = [bool]$d.active
 }
